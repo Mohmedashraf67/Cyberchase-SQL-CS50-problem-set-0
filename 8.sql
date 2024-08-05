adb94348af8f029555f020775506dbb6fa125cf7
Mohamed Ashraf <115026157+Mohmedashraf67@users.noreply.github.com>
2024-08-05 16:19:14 +0300

    Add files via upload

---
+++ 8.sql
@@ -0,0 +1,1 @@
+select count("title") as "tottal number of ep"from "episodes" where "air_date"  BETWEEN '2018-01-01' AND '2023-12-31';
---
+++ 9.sql
@@ -0,0 +1,1 @@
+select count("episode_in_season") as "episodes number" from "episodes" where "air_date" BETWEEN '2002-01-01' AND '2007-12-31'
